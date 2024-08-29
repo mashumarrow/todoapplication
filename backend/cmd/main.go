a8979f65e6f792eb1ea4bbafadeb04e9e07189ec
mashumarrow <134787738+mashumarrow@users.noreply.github.com>
2024-08-29 18:59:09 +0900

    dockerfile

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -13,12 +13,17 @@ import (
 
 
 func main() {
-    // データベースを初期化
-    //database.InitDB()
 
+    
+
+    
     // サーバーを作成
     srv := server.NewServer(database.DB, "8080")
 
     // サーバーを開始
     srv.Start()
+
+    // データベースを初期化
+    database.InitDB()
+    
 }
