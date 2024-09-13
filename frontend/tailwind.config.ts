215119214fd9c172ede5eca192d58afee0ce0391
mashumarrow <134787738+mashumarrow@users.noreply.github.com>
2024-09-14 01:44:27 +0900

    tailwindcss

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        textbrown: "#544C40",
+        brown: "#BEB4A7",
+        pink: "#EAC9C8",
+        cream: "#FFF9F1",
+        beige: "#F9F0E3",
+      },
     },
   },
   plugins: [],
